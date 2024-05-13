48c1cf4f77e5685e42fe2bdfc5bfd9277df933b4
Nikolay <Shaitor1993@mail.ru>
2024-05-13 21:40:10 +0300

    test

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app = express()
 const port = 80
 
+console.log('test commit')
+
 app.use(express.static('frontend/dist'))
 
 app.listen(port, () => console.log('Server has been started on port 80 ...'))
